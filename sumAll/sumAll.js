9e40958eac27fb005845036d30dba211896c2ff5
Logan Woolf <loganwoolf@gmail.com>
2021-08-01 01:05:59 -0600

    completed sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+// all tests pass
 
+const sumAll = function(firstNum, secondNum) {
+
+   if (typeof firstNum !== 'number' || typeof secondNum !== 'number') {
+      return 'ERROR'
+   } else if (firstNum < 0 || secondNum < 0) {
+      return 'ERROR'
+   } else {
+      let sum = firstNum + secondNum - 1;
+      let avg = (firstNum + secondNum) / 2;
+      return sum * avg;
+   }
 };
 
 module.exports = sumAll;
