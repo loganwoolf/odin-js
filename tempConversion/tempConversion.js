bcdb8e82927136701023b4def5946c78120c2eae
Logan Woolf <loganwoolf@gmail.com>
2021-08-01 20:05:39 -0600

    completed temp conversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,10 +1,25 @@
-const ftoc = function() {
+// all tests passing
 
+const ftoc = function(tempF) {
+  let tempC = tempF - 32;
+  tempC *= 5/9;
+  if (!Number.isInteger(tempC)) {
+    return +tempC.toFixed(1);
+  } else {
+    return tempC;
+  }
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  let tempF = tempC * 9 / 5;
+  tempF += 32;
+  if (!Number.isInteger(tempF)) {
+    return +tempF.toFixed(1);
+  } else {
+    return tempF;
+  }
 };
+
 
 module.exports = {
   ftoc,
