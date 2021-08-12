299832bbfbf59aee4cbd6f37517c3f53553a8e4d
Logan Woolf <loganwoolf@gmail.com>
2021-08-11 23:24:48 -0600

    completed calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	if (arr.length === 0) {
+    return 0
+  }
+  return arr.reduce( (acc, value) => acc + value )
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if (arr.length === 0) {
+    return 0
+  }
+  return arr.reduce( (acc, value) => acc * value )
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let fact = 1;
+	for (x; x > 1; x--) {
+    fact *= x
+  }
+  return fact
 };
 
 module.exports = {
