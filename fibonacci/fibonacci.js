65f185e666ab8956684b936b643270435d77358b
Logan Woolf <loganwoolf@gmail.com>
2021-08-12 00:53:15 -0600

    completed fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+   let fib = [1, 1]
+   if (n - 1 < 0) {
+      return "OOPS"
+   }
+   do {
+      fib.push(fib[fib.length - 1] + fib[fib.length - 2])
+   } while (n > fib.length)
+   return fib[+n-1]
 };
 
 module.exports = fibonacci;
